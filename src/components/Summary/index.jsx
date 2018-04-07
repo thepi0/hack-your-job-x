f824a32f88305726a68e0b3bc905f50a345d964f
Ville Jänkälä <ville.jankala@gmail.com>
2018-04-07 19:26:24 +0300

    Summary page

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
+import Header from 'Components/Header';
+import LongestDailies from 'Public/summary-group.png';
+import Offtopic from 'Public/offtopic.png';
+import DailyBar from 'Public/dailybar.gif';
 
 import './Summary.css';
 
@@ -9,9 +13,20 @@ class End extends Component {
     render() {
         return (
             <div className="Summary">
+            <Header/>
                 <div className="Summary-wrapper">
                     <h3>Daily tänään</h3>
-                    <div className="Summary-container"></div>
+                    <div className="Summary-container">
+                        <img src={DailyBar + "?"+ Math.random()} alt="Duration" className="duration" />
+                        <br/><br/><br/>
+                        <h4>Pisimmät daily-vuorot</h4>
+                        <br/>
+                        <img src={LongestDailies} alt="Longest dailies" className="smaller"/>
+                        <br/><br/><br/><br/><br/>
+                        <h4>Off-topiciin eksyttiin eniten vuoroilla</h4>
+                        <br/>
+                        <img src={Offtopic} alt="Offtopic" className="smaller"/>
+                    </div>
                 </div>
             </div>
         )
